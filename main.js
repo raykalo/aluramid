70ad09ae126b663146f2b4a9a4b494f59b3f7f22
raykalo <137127186+raykalo@users.noreply.github.com>
2023-11-27 21:51:22 -0300

    men alterando

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-function som TocaGrilo(){
-document.querySelector("som_tecla_grilo").play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play()
 }
-document.querySelector("tecla_grilo").onclick
-=somTocaGrilo;
 
+const listaDeTeclas = document.querySelectorAll (".tecla");
+
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function(){
+      tocaSom(idAudio);
+    }   
+    tecla.onkeydown = function(evento){
+      if(evento.code != 'Tab'){
+      tecla.classList.add('ativa');
+    }
+    tecla.onkeyup = function(){
+      tecla.classList.remove('ativa');
+    }
+
+       
+}
